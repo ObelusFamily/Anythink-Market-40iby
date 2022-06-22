85a28da0c798cb6e72f1e373e089072db6dc1960
yossilac <107926877+yossilac@users.noreply.github.com>
2022-06-22 15:22:25 +0300

    updated seeds.sh
    used 127.0.0.1 instead of localhost

--- backend/seeds.sh
+++ backend/seeds.sh
@@ -5,11 +5,11 @@ do
     #Create user:
     echo $i
     echo "Creating username"
-    curl -X POST http://localhost:3000/api/users -H 'Content-Type: application/json' -d '{"user":{"username":"username'${i}'","email":"username'${i}'@user.com","password":"userpass"}}'
+    curl -X POST http://127.0.0.1:3000/api/users -H 'Content-Type: application/json' -d '{"user":{"username":"username'${i}'","email":"username'${i}'@user.com","password":"userpass"}}'
 
     #Login user:
     echo "Logging in"
-    loginres=$(curl -X POST http://localhost:3000/api/users/login -H 'Content-Type: application/json' -d '{"user":{"email":"username'${i}'@user.com","password":"userpass"}}')
+    loginres=$(curl -X POST http://127.0.0.1:3000/api/users/login -H 'Content-Type: application/json' -d '{"user":{"email":"username'${i}'@user.com","password":"userpass"}}')
 
     #Get token -
     token=$(echo $loginres | python3 -c "import sys, json; print(json.load(sys.stdin)['user']['token'])")
@@ -17,7 +17,7 @@ do
     #Create item
 
     echo "Create item"
-    itemres=$(curl -X POST http://localhost:3000/api/items/ -H 'Content-Type: application/json' -H "Authorization: Bearer $token" -d '{"item":{"title":"item_'$i'","description":"description_'$i'","image":"https://res.cloudinary.com/wilco/image/upload/v1640709888/anythink-assets/bots/ness-headshot_puusqr.png","tagList":["tag1","tag2","tag3"]}}')
+    itemres=$(curl -X POST http://127.0.0.1:3000/api/items/ -H 'Content-Type: application/json' -H "Authorization: Bearer $token" -d '{"item":{"title":"item_'$i'","description":"description_'$i'","image":"https://res.cloudinary.com/wilco/image/upload/v1640709888/anythink-assets/bots/ness-headshot_puusqr.png","tagList":["tag1","tag2","tag3"]}}')
 
     echo "Get item slug name"
     #Get item -
@@ -25,7 +25,7 @@ do
     echo $item
     #Add comment
     echo "Add comment"
-    curl -X POST http://localhost:3000/api/items/$item/comments -H 'Content-Type: application/json' -H "Authorization: Bearer $token" -d '{"comment":{"body":"111comment body"}}'
+    curl -X POST http://127.0.0.1:3000/api/items/$item/comments -H 'Content-Type: application/json' -H "Authorization: Bearer $token" -d '{"comment":{"body":"111comment body"}}'
 done
 
 
